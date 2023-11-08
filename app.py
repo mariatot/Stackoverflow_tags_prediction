2e685f0c06b6f7be1000d3562b6970bd6c178f8b
mariatot <149533517+mariatot@users.noreply.github.com>
2023-11-08 11:38:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle, re, html5lib
+from bs4 import BeautifulSoup
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import tensorflow_hub as hub
+import pandas as pd
