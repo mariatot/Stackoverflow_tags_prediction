38b901fa23c687d6ccddb4349e2623b74b39511c
mariatot <149533517+mariatot@users.noreply.github.com>
2023-11-15 15:43:52 +0100

    Update app.py
    importation des librairies

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
+import tensorflow as tf
+import tensorflow_hub as hub
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
